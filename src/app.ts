cf6ec7a39c4e1a2aaa1f3827257b3235cf662dd0
Ann <lifeiwanguk@gmail.com>
2017-03-08 17:19:59 -0800

    working in general

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-
 class User{
      public scoring : number = 0;
      addScoring(point):number{
@@ -21,7 +20,8 @@ class Quiz{
 let quizArray= [
     {point:5,
     title:' 1. What is the national animal of Canada?',
-    choices:['North American beaver', 'Giant Panda', 'Kiwi'],answer:'North American beaver'},
+    choices:['North American beaver', 'Giant Panda', 'Kiwi'],
+    answer:'North American beaver'},
     {point:5,
      title:' 2. What kind of animal is the largest living creature on Earth? ', 
     choices:['Elephantwhale','whale','Dinosaur'],
@@ -45,6 +45,8 @@ let quizArray= [
 
 let paragraphs = document.getElementById('test').getElementsByTagName('p');
 let labels = document.getElementById('test').getElementsByTagName('label');
+// console.log(paragraphs);
+// console.log(radios.length);
 let j = 0;
 for(let i = 0; i < paragraphs.length; i++){ 
     let index = 0; 
@@ -62,13 +64,12 @@ let answerSentence = `Question {i}: The correct answer is the :`;
 let an = document.getElementById('answers');
 let anP = document.getElementById('plain');
 
-// an[0].innerText = '123';
-console.log(an);
 
 function myFunction() {
-   for(let i = 1; i <= quizArray.length; i++){
+   let answerIndex = 1;  
+   for(let i = 0; i < quizArray.length; i++){
      var strongArea = document.createElement('P');
-     var strongText = document.createTextNode("Answer " + i + ": ");
+     var strongText = document.createTextNode("Answer " + (answerIndex++) + ": ");
      strongArea.appendChild(strongText);
      anP.appendChild(strongArea);
 
@@ -76,5 +77,28 @@ function myFunction() {
      var pTest = document.createTextNode(quizArray[i].answer);
      pArea.appendChild(pTest);
      anP.appendChild(pArea);
+     
     }
 }
+
+let radios = document.getElementById('test').getElementsByTagName('input');
+let user = new User();
+
+function submitFunction(){
+    for(let i = 0; i < radios.length; i++){
+        for(j=0; j < quizArray.length;j++){
+            if(radios[i].checked && radios[i].value == quizArray[j].answer){
+                user.scoring += quizArray[j].point;
+            }
+        }      
+    } 
+    var pArea = document.createElement('P');
+    var pText = document.createTextNode('Your score:');
+    var strongArea = document.createElement('STRONG');
+    var storngText = document.createTextNode(''+user.scoring);
+    pArea.appendChild(pText);
+    strongArea.appendChild(storngText);
+    document.body.appendChild(pArea);
+    document.body.appendChild(strongArea);
+}
+
